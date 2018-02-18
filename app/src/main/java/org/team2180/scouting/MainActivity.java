aa55660579c4a2b3402949020cc825b10ae1073b
notsure45 <20NeyravalM@ndnj.org>
2018-02-17 23:05:32 -0500

    Local sections of app now finished.

--- app/src/main/java/org/team2180/scouting/MainActivity.java
+++ app/src/main/java/org/team2180/scouting/MainActivity.java
@@ -1,5 +1,6 @@
 package org.team2180.scouting;
 
+import android.R.drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -20,20 +21,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.team2180.scouting.listeners.DiscreteBarUpdater;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
-
 public class MainActivity extends AppCompatActivity {
+    public HashMap<String,JSONObject> teamData = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.viewContainer).setVisibility(View.GONE);
         // Initialize all bars and their labels; we need this to modify the labels based on the bar content
-        final Spinner spinner = (Spinner) findViewById(R.id.teamID);
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.team_numbers, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(adapter);
-
+        final Spinner teamSelector = (Spinner) findViewById(R.id.teamID);
+            final ArrayAdapter<CharSequence> selectorAdapter = ArrayAdapter.createFromResource(this, R.array.team_numbers, android.R.layout.simple_spinner_item);
+            teamSelector.setAdapter(selectorAdapter);
+        final Spinner reportSelector = (Spinner) findViewById(R.id.viewTeamID);
+            final ArrayAdapter<CharSequence> reportArrayAdapter = new ArrayAdapter<CharSequence>(
+                    this, android.R.layout.simple_spinner_dropdown_item, new ArrayList<CharSequence>());
+            reportArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            reportSelector.setAdapter(reportArrayAdapter);
         final SeekBar autoSwi = (SeekBar) findViewById(R.id.autoSwi);
         final SeekBar autoBal = (SeekBar) findViewById(R.id.autoBal);
         final SeekBar teleopCubeCount = (SeekBar) findViewById(R.id.teleopCubeCount);
@@ -55,33 +62,36 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.JSONoutput)).setText(text);
                 try {
                     JSONObject obj = new JSONObject(text);
-                    Iterator<?> keys = obj.keys();
-                    Log.d("========","========");
-                    while(keys.hasNext()){
-                        String key = (String)keys.next();
-                        String viewID = "view" + (key.charAt(0)+"").toUpperCase() + key.substring(1);
-
-                        View viewView = findViewById(getResources().getIdentifier(viewID,"id",MainActivity.this.getPackageName()));
-                        if(viewView instanceof CheckBox){
-                            ((CheckBox) viewView).setChecked(Boolean.valueOf(obj.get(key).toString()));
-                            viewView.setClickable(false);
-                        }else if(viewView instanceof TextView){
-                            ((TextView) viewView).setText((obj.get(key).toString()));
-                        }
-                    }
+                    int i = 0;
+                    try {
+                        while (teamData.get(obj.getString("teamID") + ":" + i) != null) {i++;}
+                    }catch(JSONException e){}
+                    teamData.put(obj.getString("teamID") + ":" + i,obj);
+                    reportArrayAdapter.add(obj.getString("teamID") + ": Report "+i);
                 }catch(JSONException e){
-
+                    Log.e("Exception",e.toString());
                 }
+                ((EditText)findViewById(R.id.comments)).setText("");
             }
         });
         swapButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                //TODO: switch views on click
+                if(findViewById(R.id.mainContainer).getVisibility()==View.VISIBLE){
+                    findViewById(R.id.mainContainer).setVisibility(View.GONE);
+                    findViewById(R.id.viewContainer).setVisibility(View.VISIBLE);
+                    ((FloatingActionButton)view).setImageResource(drawable.ic_menu_edit);
+                }else{
+                    //mainContainer is gone
+                    Log.d("viewCont",Boolean.toString(findViewById(R.id.viewComments).getVisibility()==View.VISIBLE));
+                    findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+                    findViewById(R.id.viewContainer).setVisibility(View.GONE);
+                    ((FloatingActionButton)view).setImageResource(drawable.ic_menu_info_details);
+                }
             }
         });
-        //--Team Selector--\\
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //--Team/Report Selector--\\
+        teamSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 ((TextView)findViewById(R.id.teamIDText)).setText(
@@ -93,14 +103,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        //--END TEAM SELECTOR--\\
+
+        reportSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView)findViewById(R.id.viewTeamIDText)).setText(
+                        spinnerTitle + ": " + adapterView.getItemAtPosition(i).toString());
+                    String reportID = adapterView.getItemAtPosition(i).toString().replaceAll(" Report ","");
+                    displayTeamInfo(reportID);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //--END TEAM/REPORT SELECTOR--\\
 
         // These listeners got extremely repetitive, so now they have their own class.
         // See org.team2180.scouting.listeners.DiscreteBarUpdater for more
         autoSwi.setOnSeekBarChangeListener(new DiscreteBarUpdater(autoSwi,txtAutoSwi));
         autoBal.setOnSeekBarChangeListener(new DiscreteBarUpdater(autoBal,txtAutoBal));
         teleopCubeCount.setOnSeekBarChangeListener(new DiscreteBarUpdater(teleopCubeCount,txtTeleopCubeCount));
-
+        txtAutoSwi.setText(txtAutoSwi.getText() + ": " + autoSwi.getProgress());
+        txtAutoBal.setText(txtAutoBal.getText() + ": " + autoBal.getProgress());
+        txtTeleopCubeCount.setText(txtTeleopCubeCount.getText() + ": " + teleopCubeCount.getProgress());
 
     }
     //Auto-generated
@@ -144,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
             if(subview instanceof SeekBar) {
                 compJSONobj = compJSONobj + justAComma + '"' +
                         getResources().getResourceEntryName(subview.getId())
-                        + '"' + ":" + ((SeekBar) subview).getProgress();
+                        + '"' + ":" + '"' + ((TextView)findViewById(getResources().getIdentifier(getResources().getResourceEntryName(subview.getId())+"Txt","id",MainActivity.this.getPackageName()))).getText() + '"';
                 itemsIndexed++;
             }else if(subview instanceof Spinner){
                 compJSONobj = compJSONobj + justAComma + '"' +
@@ -189,8 +216,25 @@ public class MainActivity extends AppCompatActivity {
         Byte[] byteArr = {};
         return byteArr;
     }
+    public void displayTeamInfo(String reportID){
+        JSONObject obj = teamData.get(reportID);
+        if(obj==null){Log.e("MainActivity",reportID + "is not a valid Report ID!"); return;}
+        Iterator<?> keys = obj.keys();
+        while(keys.hasNext()){
+            String key = (String)keys.next();
+            String viewID = "view" + (key.charAt(0)+"").toUpperCase() + key.substring(1);
+            View viewView = findViewById(getResources().getIdentifier(viewID,"id",MainActivity.this.getPackageName()));
 
-    public void loadJSONintoMap(){
-
+            try{
+                 if(viewView instanceof CheckBox){
+                     ((CheckBox) viewView).setChecked(Boolean.valueOf(obj.get(key).toString()));
+                     viewView.setClickable(false);
+                }else if(viewView instanceof TextView) {
+                     ((TextView) viewView).setText((obj.get(key).toString()));
+                 }
+            }catch(Exception e){
+                Log.e("EXCEPTION",e.toString());
+            }
+        }
     }
 }
